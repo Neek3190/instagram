229c47fe176590a5d30c73b1fcf9889b55338fdc
TANIA BAL <84237198+Neek3190@users.noreply.github.com>
2023-03-06 10:36:56 +0530

    home

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../widgets/feeds.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -27,43 +29,63 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.white,
+          elevation: 0,
           title: Text(
             "Instagram",
             style: GoogleFonts.fruktur(color: Colors.black, fontSize: 22),
           ),
           actions: const [
-            Icon(
-              Icons.favorite_border_outlined,
-              color: Colors.black,
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.favorite_border_outlined,
+                color: Colors.black,
+              ),
             ),
             Gap(10),
-            Icon(
-              Icons.maps_ugc_outlined,
-              color: Colors.black,
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.maps_ugc_outlined,
+                color: Colors.black,
+              ),
             ),
-            Gap(10)
+            Gap(10),
           ]),
-      body: Column(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 200,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: img.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(img[index]),
-                    radius: 30,
-                  ),
-                );
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 80,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: img.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(img[index]),
+                      radius: 40,
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-          const Text('djsfjksodfjkds')
-        ],
+            feeds(
+                'https://th.bing.com/th/id/OIP.oBMbGjn1M1keGnWzKw6-pQHaHl?pid=ImgDet&rs=1',
+                'Victor Pacheco',
+                'https://tse4.mm.bing.net/th?id=OIP.xQ22p4-a5USKTUEmnVhB9QHaEK&pid=Api&P=0'),
+            feeds(
+                'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80',
+                'Ramneek',
+                'https://images.unsplash.com/photo-1674413146432-3e01d9868828?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'),
+            feeds(
+                'https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
+                'Arshdeep',
+                'https://images.unsplash.com/photo-1674421338672-3591ba6f176e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80')
+          ],
+        ),
       ),
     );
   }
